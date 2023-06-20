dfd2ff7d89ee9cb67edc63997f30a0a4c4fc24f4
AnitaL25 <102709282+AnitaL25@users.noreply.github.com>
2023-06-20 02:48:58 -0600

    je

--- app/Views/Login.php
+++ app/Views/Login.php
@@ -1,45 +1,63 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>Login en PHP</title>
-    <meta name="viewport" content="initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <style>
-      .container{margin-top:100px}
+        body {
+            background-color: #1D5B9B;
+            padding-top: 100px;
+            align:center;
+        }
+
+        .container {
+            max-width: 400px;
+            background-color: #00FFFF;
+            padding: 20px;
+            border-radius: 5px;
+        }
+
+        .container h2 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+
+        .btn-login {
+            width: 100%;
+        }
+
+        .btn-action {
+            margin-top: 10px;
+        }
     </style>
-  </head>
+</head>
 
-  <body style="background-color: #1D5B9B;">
-
+<body>
     <div class="container">
-    <h2>Login</h2>
-    <br><br>
-      <form class="form-horizontal" action="login.php" method="post">
-        <div class="form-group">
-          <label for="inputEmail3" class="col-sm-2 control-label">Usuario</label>
-          <div class="col-sm-4">
-            <input type="text" class="form-control" name="usuario" id="inputusuario" placeholder="Usuario" required>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-          <div class="col-sm-4">
-            <input type="password" class="form-control" name="password" id="inputPassword3" placeholder="Password" required>
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">entrar</button>
-          </div>
-        </div>
+        <h2>Login</h2>
 
-        <form class="form-inline my-2 my-lg-0">          
-          <a href="http://localhost/ecommerce/home" class="btn btn-primary">home</a>
-          </form>
-      </form>
+        <form action="/login/processLogin" method="post">
+            <div class="form-group">
+                <label for="inputUsuario">Usuario</label>
+                <input type="text" class="form-control" id="inputUsuario" name="usuario" placeholder="Usuario" required>
+            </div>
+
+            <div class="form-group">
+                <label for="inputPassword">Contraseña</label>
+                <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Contraseña" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-login">Entrar</button>
+        </form>
+
+        <div class="text-center">
+            <a href="http://localhost/ecommerce/home" class="btn btn-primary btn-action">Home</a>
+            <a href="http://localhost/ecommerce/cliente" class="btn btn-primary btn-action">Registrar</a>
+        </div>
     </div>
 
-  </body>
+</body>
 </html>
